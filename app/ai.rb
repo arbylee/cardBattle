16aa5bbfbf025afbe23b0433fc187c90c20bcc1b
Richard Lee <rblee88@gmail.com>
2015-12-27 13:46:58 -0600

    Add AI player

---
+++ app/ai.rb
@@ -0,0 +1,15 @@
+class Ai < Player
+  def get_next_battle_card
+    choices = []
+    3.times do
+      choices << @deck.get_next_card
+    end
+
+    selected = choices.slice! [0,1,2].sample
+    choices.each do |leftover_card|
+      discard leftover_card
+    end
+
+    return selected
+  end
+end
--- app/game.rb
+++ app/game.rb
@@ -1,15 +1,16 @@
 require_relative "player"
+require_relative "ai"
 require_relative "attack_card"
 require_relative "battle"
 class Game
   def initialize
     @player1 = Player.new
-    20.times do
+    10.times do
       @player1.add_card AttackCard.new
     end
 
-    @player2 = Player.new
-    20.times do
+    @player2 = Ai.new
+    10.times do
       @player2.add_card AttackCard.new
     end
   end
